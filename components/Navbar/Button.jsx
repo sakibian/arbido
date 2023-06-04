c212ebc85deb196876e62be801f571f1acb10253
shahinul islam <shahinul37@outlook.com>
2023-06-04 18:59:31 +0600

    footer added

--- components/Navbar/Button.jsx
+++ components/Navbar/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ children, props }) => {
 	return (
-		<button className="h-14 w-52 bg-[#01E81F] text-white tracking-[5px]">
+		<button className="h-14 w-52 bg-black text-white tracking-[5px]">
 			{children}
 		</button>
 	);
